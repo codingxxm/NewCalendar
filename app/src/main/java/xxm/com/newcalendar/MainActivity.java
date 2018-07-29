3f55ac2fba26bc32f8275de21842cf37578427d6
xuxm <xuxm@primeton.com>
2018-07-29 17:23:06 +0800

    Project Initial commit

---
+++ app/src/main/java/xxm/com/newcalendar/MainActivity.java
@@ -0,0 +1,13 @@
+package xxm.com.newcalendar;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/xxm/com/newcalendar/NewCalendar.java
@@ -0,0 +1,20 @@
+package xxm.com.newcalendar;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.widget.LinearLayout;
+
+public class NewCalendar extends LinearLayout {
+    public NewCalendar(Context context) {
+        super(context);
+    }
+
+    public NewCalendar(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public NewCalendar(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
